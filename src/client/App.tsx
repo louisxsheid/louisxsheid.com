332602a881b514e47490c7e9e615b7e30d888540
Louis Xavier Sheid III <louisxsheid@gmail.com>
2021-04-27 17:31:54 -0700

    move fade-in to app.tsx

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import FadeIn from 'react-fade-in';
 import "./styles/App.scss"
 import NavBar from './components/navbar';
 import WhoAmI from './components/AboutMePage/WhoAmI';
@@ -7,22 +8,29 @@ import Experience from './components/AboutMePage/Experience';
 
 const App = (): JSX.Element => {
   return (
-    <div className='wrapper'>
-      <div style={{paddingRight: '5rem'}}>
-        <NavBar />
-      </div>
-      <div className="whoami-skills-wrapper">
+      <div className='wrapper'>
         <div style={{paddingRight: '5rem'}}>
-          <WhoAmI />
+          <NavBar />
+        </div>
+        <div className="whoami-skills-wrapper">
+          <div style={{paddingRight: '5rem'}}>
+            <FadeIn>
+              <WhoAmI />
+            </FadeIn>
+          </div>
+          <div style={{paddingRight: '5rem'}}>
+            <FadeIn>
+              <Skills />
+            </FadeIn>
+          </div>
         </div>
         <div style={{paddingRight: '5rem'}}>
-          <Skills />
+          <FadeIn>
+            <Experience />
+          </FadeIn>
         </div>
+        
       </div>
-      <div style={{paddingRight: '5rem'}}>
-        <Experience />
-      </div>
-    </div>
   );
 }
 
--- src/client/components/AboutMePage/WhoAmI.tsx
+++ src/client/components/AboutMePage/WhoAmI.tsx
@@ -1,44 +1,41 @@
 import React from 'react';
 const description = "I am a software developer experienced in creating full stack javascript / typescript applications. Currently learning anything I can."
-import FadeIn from 'react-fade-in';
 
 const WhoAmI = (): JSX.Element => {
   return (
-    <FadeIn>
-    <div className="whoami-wrapper">
-      <div>
-        <div className="title">
-          Louis Xavier Sheid III
-        </div>
-        <img src={require("../../../../public/assets/louis.PNG")}/>
-        <div className='icon-wrapper'>
-          <div>💻</div> <div style={{paddingLeft: '0.25rem'}}>Software Developer</div>
-        </div>
-        <div className='icon-wrapper'>
-          <div>🌴</div> <div style={{paddingLeft: '0.38rem'}}>San Diego, California</div>
-        </div>
-        <div style={{padding: '0.5rem'}}/>
-        <div style={{width: '15rem'}}>
-          {description}
-        </div>
-        <div style={{paddingTop: '1rem'}}>
-          @louisxsheid
-        </div>
-        <div className="icon-wrapper">
-          <div className='social-wrapper'>
-            <a href='https://www.github.com/louisxsheid' target="_blank" rel="noreferrer">
-              <img className="icon" src={require("../../../../public/assets/github.png")} />
-            </a>
+      <div className="whoami-wrapper">
+        <div>
+          <div className="title">
+            Louis Xavier Sheid III
           </div>
-          <div className='social-wrapper'>
-            <a href='https://www.linkedin.com/in/louisxsheid/' target="_blank" rel="noreferrer">
-              <img className="icon" src={require("../../../../public/assets/linkedin.png")} />
-            </a>
+          <img src={require("../../../../public/assets/louis.PNG")}/>
+          <div className='icon-wrapper'>
+            <div>💻</div> <div style={{paddingLeft: '0.25rem'}}>Software Developer</div>
+          </div>
+          <div className='icon-wrapper'>
+            <div>🌴</div> <div style={{paddingLeft: '0.38rem'}}>San Diego, California</div>
+          </div>
+          <div style={{padding: '0.5rem'}}/>
+          <div style={{width: '15rem'}}>
+            {description}
+          </div>
+          <div style={{paddingTop: '1rem'}}>
+            @louisxsheid
+          </div>
+          <div className="icon-wrapper">
+            <div className='social-wrapper'>
+              <a href='https://www.github.com/louisxsheid' target="_blank" rel="noreferrer">
+                <img className="icon" src={require("../../../../public/assets/github.png")} />
+              </a>
+            </div>
+            <div className='social-wrapper'>
+              <a href='https://www.linkedin.com/in/louisxsheid/' target="_blank" rel="noreferrer">
+                <img className="icon" src={require("../../../../public/assets/linkedin.png")} />
+              </a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    </FadeIn>
   )
 }
 
